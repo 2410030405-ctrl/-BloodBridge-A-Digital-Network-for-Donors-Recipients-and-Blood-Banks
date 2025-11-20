0d95756c2454e59ffdd63e0c6436daaefbd91a3f
2410030405-ctrl <2410030405@klh.edu.in>
2025-11-20 22:35:07 +0530

    Delete frontend directory

--- frontend/src/pages/RequestPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function RequestPage() {
-  return (
-    <div className="p-6 text-center text-white">
-      <h1 className="text-3xl font-bold text-green-400 mb-2">Request Blood</h1>
-      <p>Coming soon: Submit a blood request form.</p>
-    </div>
-  );
-}
